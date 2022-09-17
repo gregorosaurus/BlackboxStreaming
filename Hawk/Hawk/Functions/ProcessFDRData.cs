54a302253d2898137c27de2005395089c056305d
Greg Mardon <greg@MacbookAirM2.local>
2022-09-17 15:37:39 -0600

    Initial hawk commit

---
+++ Hawk/Hawk/Functions/ProcessFDRData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Azure.Messaging.EventHubs;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Hawk.Functions
+{
+    public class ProcessFDRData
+    {
+        [FunctionName("ProcessFDRData")]
+        public static async Task Run([EventHubTrigger("evt-blackbox-fdrraw", Connection = "EventHubConnectionString")] EventData[] events, ILogger log)
+        {
+            var exceptions = new List<Exception>();
+
+            foreach (EventData eventData in events)
+            {
+                try
+                {
+                    // Replace these two lines with your processing logic.
+                    log.LogInformation($"C# Event Hub trigger function processed a message: {eventData.EventBody}");
+                    await Task.Yield();
+                }
+                catch (Exception e)
+                {
+                    exceptions.Add(e);
+                }
+            }
+
+            // Once processing of the batch is complete, if any messages in the batch failed processing throw an exception so that there is a record of the failure.
+
+            if (exceptions.Count > 1)
+                throw new AggregateException(exceptions);
+
+            if (exceptions.Count == 1)
+                throw exceptions.Single();
+        }
+    }
+}
+
---
+++ Hawk/Hawk/Models/RawDataMessage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Hawk.Models
+{
+    public class RawDataMessage
+    {
+        public string? AircraftIdentifier { get; set; }
+        public DateTime ProcessedAt { get; set; } = DateTime.UtcNow;
+        /// <summary>
+        /// base64 encoded subframe data
+        /// </summary>
+        public string? SubframeData { get; set; }
+
+        public byte[] SubframeBinaryData
+        {
+            get
+            {
+                if (SubframeData == null)
+                    return new byte[0];
+
+                return Convert.FromBase64String(SubframeData!);
+            }
+        }
+    }
+}
+

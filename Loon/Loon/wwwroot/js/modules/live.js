f348979ea468faface1559033fe848a4eae73ef4
Greg Mardon <greg@MacbookAirM2.local>
2022-09-17 20:39:34 -0600

    Map displaying, no connection still to Eventhub

---
+++ Loon/Loon/wwwroot/js/modules/live.js
@@ -0,0 +1,27 @@
+﻿export var name = "live";
+
+var _map, _marker;
+
+export function initMap(subscriptionKey) {
+    _map = new atlas.Map('map-area', {
+        center: [-114, 51],
+        style: 'grayscale_dark',
+        zoom: 12,
+        language: 'en-US',
+        authOptions: {
+            authType: 'subscriptionKey',
+            subscriptionKey: subscriptionKey
+        }
+    });
+
+
+    _map.events.add('ready', function () {
+        //Create a HTML marker and add it to the map.
+        _marker = new atlas.HtmlMarker({
+            htmlContent: '<div class="pulseIcon"></div>',
+            position: [-114, 51]
+        });
+
+        _map.markers.add(_marker);
+    });
+}
